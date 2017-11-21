574ee0fcd26471d43c47a62479b7a5aa505d66d2
dingzhexue <dingzhexue@126.com>
2017-11-20 22:37:34 -0800

    Fix gray issue

--- TeamTap/TTTeamViewController.m
+++ TeamTap/TTTeamViewController.m
@@ -19,6 +19,8 @@
 - (void)getGame;
 - (void)checkGameStart;
 - (UIImage *)convertImageToGrayScale:(UIImage *)image;
+- (UIImage *) convertToGreyscale:(UIImage *)i;
+
 @end
 
 @implementation TTTeamViewController
@@ -161,8 +163,8 @@ int secondsLeft;
         [self.teamaButton setImage:self.originalTeamaImage forState:UIControlStateNormal];
     }
     
-//    UIImage *newImage = [self convertImageToGrayScale:self.originalTeambImage];
-    UIImage *newImage = [UIImage imageNamed:@"grey8.png"];
+    UIImage *newImage = [self convertToGreyscale:self.originalTeambImage];
+//    UIImage *newImage = [UIImage imageNamed:@"grey8.png"];
     [self.teambButton setImage:newImage forState:UIControlStateNormal];
     
     self.selectedTeam = @"a";
@@ -174,8 +176,8 @@ int secondsLeft;
         [self.teambButton setImage:self.originalTeambImage forState:UIControlStateNormal];
     }
     
-//    UIImage *newImage = [self convertImageToGrayScale:self.originalTeamaImage];
-    UIImage *newImage = [UIImage imageNamed:@"grey7.png"];
+    UIImage *newImage = [self convertToGreyscale:self.originalTeamaImage];
+//    UIImage *newImage = [UIImage imageNamed:@"grey7.png"];
     [self.teamaButton setImage:newImage forState:UIControlStateNormal];
     
     self.selectedTeam = @"b";
@@ -208,6 +210,68 @@ int secondsLeft;
     
     // Return the new grayscale image
     return outlineImage;
+}
+
+- (UIImage *) convertToGreyscale:(UIImage *)i {
+    
+    int kRed = 1;
+    int kGreen = 2;
+    int kBlue = 4;
+    
+    int colors = kGreen | kBlue | kRed;
+    int m_width = i.size.width;
+    int m_height = i.size.height;
+    
+    uint32_t *rgbImage = (uint32_t *) malloc(m_width * m_height * sizeof(uint32_t));
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(rgbImage, m_width, m_height, 8, m_width * 4, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaNoneSkipLast);
+    CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
+    CGContextSetShouldAntialias(context, NO);
+    CGContextDrawImage(context, CGRectMake(0, 0, m_width, m_height), [i CGImage]);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    
+    // now convert to grayscale
+    uint8_t *m_imageData = (uint8_t *) malloc(m_width * m_height);
+    for(int y = 0; y < m_height; y++) {
+        for(int x = 0; x < m_width; x++) {
+            uint32_t rgbPixel=rgbImage[y*m_width+x];
+            uint32_t sum=0,count=0;
+            if (colors & kRed) {sum += (rgbPixel>>24)&255; count++;}
+            if (colors & kGreen) {sum += (rgbPixel>>16)&255; count++;}
+            if (colors & kBlue) {sum += (rgbPixel>>8)&255; count++;}
+            m_imageData[y*m_width+x]=sum/count;
+        }
+    }
+    free(rgbImage);
+    
+    // convert from a gray scale image back into a UIImage
+    uint8_t *result = (uint8_t *) calloc(m_width * m_height *sizeof(uint32_t), 1);
+    
+    // process the image back to rgb
+    for(int i = 0; i < m_height * m_width; i++) {
+        result[i*4]=0;
+        int val=m_imageData[i];
+        result[i*4+1]=val;
+        result[i*4+2]=val;
+        result[i*4+3]=val;
+    }
+    
+    // create a UIImage
+    colorSpace = CGColorSpaceCreateDeviceRGB();
+    context = CGBitmapContextCreate(result, m_width, m_height, 8, m_width * sizeof(uint32_t), colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaNoneSkipLast);
+    CGImageRef image = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    UIImage *resultUIImage = [UIImage imageWithCGImage:image];
+    CGImageRelease(image);
+    
+    free(m_imageData);
+    
+    // make sure the data will be released by giving it to an autoreleased NSData
+    [NSData dataWithBytesNoCopy:result length:m_width * m_height];
+    
+    return resultUIImage;
 }
 
 #pragma mark - API
